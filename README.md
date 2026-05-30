d6647580325cf975b54b4dcc29e45505b8c4da22
Lifar' Anton <lifaranton@gmail.com>
2026-05-30 16:03:24 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Name
+
+Simple landing page
+
+## Tech stack
+
+- HTML
+- CSS
+- JavaScript
---
+++ js/burger.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__menu");
+burger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+
+  const isActive = menu.classList.contains("active");
+  burger.setAttribute("aria-expanded", `${isActive}`);
+})
